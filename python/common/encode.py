c4927a15fb5f0a40eb0fb177f603d31b1d6d89d6
RR <108212077+0xRobinR@users.noreply.github.com>
2026-08-20 23:34:14 +0530

    add: break maths

--- python/common/encode.py
+++ python/common/encode.py
@@ -14,17 +14,27 @@ print(x.to_bytes(2, "little").hex())
 def test_encode(value: int) -> bytes:
     return value.to_bytes(2, "big")
 
-def test_decode(data: bytes) -> int:
-    return int.from_bytes(data)
+def encode_from_maths(value: int) -> bytes:
+    assert value < 2**256
+    high = value // 256
+    low = value % 256
 
-print(test_encode(0).hex() == "0000")
-print(test_encode(1).hex() == "0001")
-print(test_encode(255).hex() == "00ff")
-print(test_encode(256).hex() == "0100")
-print(test_encode(65535).hex() == "ffff")
+    print(high, low)
+
+    return bytes([high, low])
+
+def test_decode(data: bytes) -> int:
+    assert len(data) == 2
+    return int.from_bytes(data, "big")
+
+print(encode_from_maths(0).hex() == "0000")
+print(encode_from_maths(1).hex() == "0001")
+print(encode_from_maths(10).hex())
+print(encode_from_maths(256).hex() == "0100")
+print(encode_from_maths(65535).hex() == "ffff")
 
 try:
-    print(test_encode(65536))
+    print(encode_from_maths(65536))
 except Exception as e:
     print("I knew it")
 
@@ -35,4 +45,8 @@ assert test_decode(bytes.fromhex("00ff")) == 255
 assert test_decode(bytes.fromhex("0100")) == 256
 assert test_decode(bytes.fromhex("ffff")) == 65535
 
-print(test_decode(bytes.fromhex("010000")))
+# print(test_decode(bytes.fromhex("010000")))
+
+print(test_decode(b""))
+print(test_decode(b"\x01"))
+print(test_decode(b"\x01\x02\x03"))

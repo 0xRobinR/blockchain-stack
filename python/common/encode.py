9c8307746554dc82979d96f8f73c7fb8e066411e
RR <108212077+0xRobinR@users.noreply.github.com>
2026-08-20 22:47:56 +0530

    add: decode

--- python/common/encode.py
+++ python/common/encode.py
@@ -14,6 +14,8 @@ print(x.to_bytes(2, "little").hex())
 def test_encode(value: int) -> bytes:
     return value.to_bytes(2, "big")
 
+def test_decode(data: bytes) -> int:
+    return int.from_bytes(data)
 
 print(test_encode(0).hex() == "0000")
 print(test_encode(1).hex() == "0001")
@@ -25,3 +27,12 @@ try:
     print(test_encode(65536))
 except Exception as e:
     print("I knew it")
+
+
+assert test_decode(bytes.fromhex("0000")) == 0
+assert test_decode(bytes.fromhex("0001")) == 1
+assert test_decode(bytes.fromhex("00ff")) == 255
+assert test_decode(bytes.fromhex("0100")) == 256
+assert test_decode(bytes.fromhex("ffff")) == 65535
+
+print(test_decode(bytes.fromhex("010000")))
